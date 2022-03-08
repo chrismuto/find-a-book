da9697ebeb754c09f0565d69d811123eaa92ea99
chrismuto <chrismuto@gmail.com>
2022-03-07 20:20:24 -0800

    created schema file and subfolders

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// const { ApolloServer } = require('apollo-server-express');
+// const { typeDefs, resolvers } = require('./schemas');
+// const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +18,24 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+// const startApolloServer = async (typeDefs, resolvers) => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware,
+//   });
+  
+//   await server.start();
+//   server.applyMiddleware({ app });
+  
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//     })
+//   })
+// };
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
