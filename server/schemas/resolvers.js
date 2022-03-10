34f94c944d6555bd891835426af513e8aa169acc
chrismuto <chrismuto@gmail.com>
2022-03-09 21:56:59 -0800

    save book function works on backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,54 +1,86 @@
-const { AuthenticationError, UserInputError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken, authMiddleware } = require('../utils/auth');
+const {
+  AuthenticationError,
+  UserInputError
+} = require('apollo-server-express');
+const {
+  User
+} = require('../models');
+const {
+  signToken,
+  authMiddleware
+} = require('../utils/auth');
 
 const resolvers = {
-    Mutation: {
-      addUser: async (parent, { username, email, password }) => {
-        try {
-        const user = await User.create({ username, email, password });
+  Mutation: {
+    addUser: async (parent, {
+      username,
+      email,
+      password
+    }) => {
+      try {
+        const user = await User.create({
+          username,
+          email,
+          password
+        });
         const token = await signToken(user);
-        return { token, user };
-        }
-        catch (error) {
-          console.log(error);
-          throw new UserInputError("Please provide unique username and email")
-        }
-      },
-
-      saveBook: async (parent, { book }, context) => {
-        console.log(context.user)
-        console.log(book)
-        if (context.user) {
-          return await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $push: { savedBooks: { book } } },
-            {
-              new: true,
-              runValidators: true,
-            }
-          );
-        }
-      },
-
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-  
-        if (!user) {
-          throw new AuthenticationError('No user found with this email address');
-        }
-  
-        const correctPw = await user.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-  
-        const token = signToken(user);
-  
-        return { token, user };
-      },
+        return {
+          token,
+          user
+        };
+      } catch (error) {
+        console.log(error);
+        throw new UserInputError("Please provide unique username and email")
+      }
     },
+
+    saveBook: async (parent, {authors, description, bookId, image, link, title} , context) => {
+      console.log(context.user)
+      if (context.user) {
+        return await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: { 
+            authors,
+            description,
+            bookId,
+            image,
+            link,
+            title } } },
+          {
+            new: true,
+            runValidators: true
+          }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in to save your books!");
+    },
+
+    login: async (parent, {
+      email,
+      password
+    }) => {
+      const user = await User.findOne({
+        email
+      });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return {
+        token,
+        user
+      };
+    },
+  },
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,27 +8,24 @@ type Query {
 type Mutation {
   addUser(username: String!, email: String!, password: String!): Auth
   login(email: String!, password: String!): Auth
-  saveBook(book: BookData): User
+  saveBook(
+    authors: [String],
+    description: String,
+    bookId: String,
+    image: String,
+    link: String,
+    title: String,
+    ): User
 }
 
 type Book {
   _id: ID
   authors: [String]
-  description: String!
+  description: String
   image: String
   link: String
-  bookId: String!
-  title: String!
-}
-
-input BookData {
-  _id: ID
-  authors: [String]
-  description: String!
-  image: String
-  link: String
-  bookId: String!
-  title: String!
+  bookId: String
+  title: String
 }
 
 type Auth {
